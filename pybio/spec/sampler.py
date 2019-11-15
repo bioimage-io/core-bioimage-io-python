290d79ee701af3e7a00fdc0aaef17963e88aabd8
fynnbe <thefynnbe@gmail.com>
2019-11-15 23:32:22 +0100

    correct cite for BroadNucleusData.reader

--- pybio/spec/sampler.py
+++ pybio/spec/sampler.py
@@ -1,5 +1,10 @@
+from typing import Any, Dict, List, Union
+
 from pybio.spec import CommonSpec
 
 
 class SamplerSpec(CommonSpec):
-    pass
+    def __init__(self, outputs: Union[str, List[Dict[str, Any]]], **super_kwargs):
+        assert isinstance(outputs, str) or isinstance(outputs, list), type(outputs)
+        super().__init__(**super_kwargs)
+        self.outputs = outputs
