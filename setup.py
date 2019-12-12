28c5c4536f1a280d45699c83f62ffecc7a109cea
Maksim Novikov <mnovikov.work@gmail.com>
2019-12-12 13:27:07 +0100

    Add setup.py and tox.ini

---
+++ setup.py
@@ -0,0 +1,36 @@
+from io import open
+from os import path
+
+from setuptools import find_namespace_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="pybio",
+    version="0.1a",
+    description="Parser library for bioimage model zoo specs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bioimage-io/python-bioimage-io",
+    author="Bioimage Team",
+    classifiers=[  # Optional
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_namespace_packages(exclude=["tests"]),  # Required
+    install_requires=["marshmallow>=3.3.0", "PyYAML>=5.2"],
+    extras_require={
+        "core": ["requests", "numpy", "sklearn", "imageio"],
+        "test": ["pytest", "tox"],
+    },
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/bioimage-io/python-bioimage-io/issues",
+        "Source": "https://github.com/bioimage-io/python-bioimage-io",
+    },
+)
