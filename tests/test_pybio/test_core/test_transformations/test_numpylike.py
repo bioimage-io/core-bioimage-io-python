41fa0694b115d7ef4247a1aa2ac1acc9a6c5f963
fynnbe <thefynnbe@gmail.com>
2020-01-16 13:14:59 +0100

    remove numpylike Reshape test

--- tests/test_pybio/test_core/test_transformations/test_numpylike.py
+++ tests/test_pybio/test_core/test_transformations/test_numpylike.py
@@ -1,18 +1,7 @@
 import numpy
 import pytest
 
-from pybio.core.transformations.numpylike import Reshape, Transpose
-
-
-reshape_testdata = [((12,), (3, 4), (12,)), ((-1,), (3, 4), (12,)), ((6, -1), (1, 2, 3, 4), (6, 4))]
-
-
-@pytest.mark.parametrize("newshape,ipt_shape,out_shape", reshape_testdata)
-def test_reshape(newshape, ipt_shape, out_shape):
-    trf = Reshape(newshape=newshape)
-    ipt = numpy.empty(ipt_shape)
-    out = trf.apply(ipt)[0]
-    assert out.shape == out_shape
+from pybio.core.transformations.numpylike import Transpose
 
 
 transpose_testdata = [(None, (3, 4), (4, 3)), ((1, 0), (3, 4), (4, 3)), ((0, 1), (3, 4), (3, 4))]
