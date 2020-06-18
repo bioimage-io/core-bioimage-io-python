26302b6548b0d64c4a5c4507afb016a7da140418
fynnbe <thefynnbe@gmail.com>
2020-06-18 10:35:53 +0200

    make json.dump pretty

--- scripts/generate_json_specs.py
+++ scripts/generate_json_specs.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-from marshmallow_jsonschema import JSONSchema
+from marshmallow_jsonschema import JSONSchema  # todo: add marshmallow_jsonschema to dependencies
 
 from pybio.spec.schema import ModelSpec
 
@@ -10,4 +10,4 @@ JSON_SPEC_PATH = Path(__file__).parent / "../json_specs"
 model_schema = ModelSpec()
 
 with (JSON_SPEC_PATH / "model_spec.json").open("w") as f:
-    json.dump(JSONSchema().dump(model_schema), f)
+    json.dump(JSONSchema().dump(model_schema), f, indent=4, sort_keys=True)
