93cf26616aa543e7fd958c1ff51598787416d1ba
fynnbe <thefynnbe@gmail.com>
2020-11-27 14:51:35 +0100

    remove local paths and skip writing test data

--- pybio/sklearn/training/classic_fit.py
+++ pybio/sklearn/training/classic_fit.py
@@ -10,7 +10,6 @@ from pybio.spec.utils import get_instance
 from pybio.spec.utils.transformers import load_and_resolve_spec
 
 
-
 test_input_path = "/repos/python-bioimage-io/specs/models/sklearn/test_input_raw.npy"
 test_output_path = "/repos/python-bioimage-io/specs/models/sklearn/test_output.npy"
 
@@ -21,13 +20,16 @@ def classic_fit(spec: Model):
     dataset = BroadNucleusDataBinarized()
     batch = dataset[(slice(None), slice(100, 110), slice(100, 110))]
 
-    test_input = dataset[(slice(0, 1), slice(100, 200), slice(100, 200))]["x"].reshape((-1, 1))
-    numpy.save(test_input_path, test_input)
     assert Path(test_input_path).exists()
     model.fit(batch["x"].reshape((-1, 1)), batch["y"].reshape((-1, 1)))
 
-    test_output = model(test_input)
-    numpy.save(test_output_path, test_output)
+    write_test_inputs_and_outputs = False
+    if write_test_inputs_and_outputs:
+        test_input = dataset[(slice(0, 1), slice(100, 200), slice(100, 200))]["x"].reshape((-1, 1))
+        numpy.save(test_input_path, test_input)
+        test_output = model(test_input)
+        numpy.save(test_output_path, test_output)
+
     return model
 
 
@@ -36,7 +38,7 @@ def create_rf_weights():
         Path(__file__).parent / "../../../specs/models/sklearn/RandomForestClassifier.model.yaml"
     )
     model = classic_fit(spec)
-    with Path("/repos/python-bioimage-io/specs/models/sklearn/rf_v0.pickle").open("wb") as f:
+    with (Path(__file__).parent / "../../../specs/models/sklearn/rf_v0.pickle").open("wb") as f:
         pickle.dump(model, f)
 
 
@@ -46,6 +48,7 @@ def restore_rf_from_weights():
     )
     with spec.weights["pickle"].source.open("br") as f:
         model = pickle.load(f)
+
 
 if __name__ == "__main__":
     create_rf_weights()
