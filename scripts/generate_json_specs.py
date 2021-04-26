6448a43f9f352ac0191232126a5553284687fa9b
fynnbe <thefynnbe@gmail.com>
2021-04-26 15:34:05 +0200

    change name of and update json schema

--- scripts/generate_json_specs.py
+++ scripts/generate_json_specs.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from marshmallow_jsonschema import JSONSchema  # todo: add marshmallow_jsonschema to dependencies
 
+import pybio.spec
 from pybio.spec.schema import Model
 
 JSON_SPEC_PATH = Path(__file__).parent / "../json_specs"
@@ -11,7 +12,7 @@ JSON_SPEC_PATH = Path(__file__).parent / "../json_specs"
 def generate_json_model_spec():
     model_schema = Model()
 
-    with (JSON_SPEC_PATH / "model_spec.json").open("w") as f:
+    with (JSON_SPEC_PATH / f"generated_model_spec_{pybio.spec.__version__}.json").open("w") as f:
         json_schema = JSONSchema().dump(model_schema)
         json.dump(json_schema, f, indent=4, sort_keys=True)
 
