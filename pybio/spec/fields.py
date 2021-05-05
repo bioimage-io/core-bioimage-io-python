39d59113834726d6d4d382355c6be8caa97cb8a6
fynnbe <thefynnbe@gmail.com>
2021-05-05 20:05:15 +0200

    sort and document fields

--- pybio/spec/fields.py
+++ pybio/spec/fields.py
@@ -16,6 +16,8 @@ from pybio.spec.exceptions import PyBioValidationException
 
 
 class DocumentedField:
+    """base class for all fields that aids in generating a documentation"""
+
     def __init__(
         self,
         *super_args,
@@ -29,11 +31,62 @@ class DocumentedField:
             bases.insert(0, self.__class__.__name__)
 
         self.type_name = "→".join(bases)
-        # self.type_name = bases[-1]
         self.bioimageio_description = bioimageio_description
         self.bioimageio_description_order = bioimageio_description_order
         self.bioimageio_maybe_required = bioimageio_maybe_required
         super().__init__(*super_args, **super_kwargs)
+
+
+#################################################
+# fields directly derived from marshmallow fields
+#################################################
+
+
+class Array(DocumentedField, marshmallow_fields.Field):
+    def __init__(self, inner: marshmallow_fields.Field, **kwargs):
+        self.inner = inner
+        super().__init__(**kwargs)
+
+    @property
+    def dtype(self) -> typing.Union[typing.Type[int], typing.Type[float], typing.Type[str]]:
+        if isinstance(self.inner, Integer):
+            return int
+        elif isinstance(self.inner, Float):
+            return float
+        elif isinstance(self.inner, String):
+            return str
+        else:
+            raise NotImplementedError(self.inner)
+
+    def _deserialize_inner(self, value):
+        if isinstance(value, list):
+            return [self._deserialize_inner(v) for v in value]
+        else:
+            return self.inner.deserialize(value)
+
+    def deserialize(self, value: typing.Any, attr: str = None, data: typing.Mapping[str, typing.Any] = None, **kwargs):
+        value = self._deserialize_inner(value)
+
+        if isinstance(value, list):
+            try:
+                return numpy.array(value, dtype=self.dtype)
+            except ValueError as e:
+                raise PyBioValidationException(str(e)) from e
+        else:
+            return value
+
+
+class DateTime(DocumentedField, marshmallow_fields.DateTime):
+    """
+    Parses datetime in ISO8601 or if value already has datetime.datetime type
+    returns this value
+    """
+
+    def _deserialize(self, value, attr, data, **kwargs):
+        if isinstance(value, datetime.datetime):
+            return value
+
+        return super()._deserialize(value, attr, data, **kwargs)
 
 
 class Dict(DocumentedField, marshmallow_fields.Dict):
@@ -81,46 +134,6 @@ class String(DocumentedField, marshmallow_fields.String):
     pass
 
 
-class Union(DocumentedField, marshmallow_union.Union):
-    def __init__(self, *super_args, **super_kwargs):
-        super().__init__(*super_args, **super_kwargs)
-        self.type_name += f"\[{' | '.join(cf.type_name for cf in self._candidate_fields)}\]"  # add types of options
-
-
-if typing.TYPE_CHECKING:
-    import pybio.spec.schema
-
-
-class StrictVersion(String):
-    def _deserialize(
-        self,
-        value: typing.Any,
-        attr: typing.Optional[str],
-        data: typing.Optional[typing.Mapping[str, typing.Any]],
-        **kwargs,
-    ):
-        return distutils.version.StrictVersion(str(value))
-
-    # def _serialize(self, value: typing.Any, attr: str, obj: typing.Any, **kwargs):
-    #     return str(value)
-
-    # def _jsonschema_type_mapping(self):
-    #     return {"type": "string"}  # todo: improve json schema of StrictVersion
-
-
-class DateTime(DocumentedField, marshmallow_fields.DateTime):
-    """
-    Parses datetime in ISO8601 or if value already has datetime.datetime type
-    returns this value
-    """
-
-    def _deserialize(self, value, attr, data, **kwargs):
-        if isinstance(value, datetime.datetime):
-            return value
-
-        return super()._deserialize(value, attr, data, **kwargs)
-
-
 class Tuple(DocumentedField, marshmallow_fields.Tuple):
     def _jsonschema_type_mapping(self):
         import marshmallow_jsonschema
@@ -131,47 +144,45 @@ class Tuple(DocumentedField, marshmallow_fields.Tuple):
         }
 
 
-class SpecURI(Nested):
-    def _deserialize(self, value, attr, data, **kwargs):
-        uri = urlparse(value)
-
-        if uri.query:
-            raise PyBioValidationException(f"Invalid URI: {uri}. Got URI query: {uri.query}")
-        if uri.fragment:
-            raise PyBioValidationException(f"Invalid URI: {uri}. Got URI fragment: {uri.fragment}")
-        if uri.params:
-            raise PyBioValidationException(f"Invalid URI: {uri}. Got URI params: {uri.params}")
-
-        # account for leading '/' for windows paths, e.g. '/C:/folder'
-        # see https://stackoverflow.com/questions/43911052/urlparse-on-a-windows-file-scheme-uri-leaves-extra-slash-at-start
-        path = url2pathname(uri.path)
-
-        return raw_nodes.SpecURI(spec_schema=self.schema, scheme=uri.scheme, netloc=uri.netloc, path=path, query="")
+class Union(DocumentedField, marshmallow_union.Union):
+    def __init__(self, *super_args, **super_kwargs):
+        super().__init__(*super_args, **super_kwargs)
+        self.type_name += f"\[{' | '.join(cf.type_name for cf in self._candidate_fields)}\]"  # add types of options
 
 
-class URI(String):
-    def _deserialize(self, *args, **kwargs) -> raw_nodes.URI:
-        uri_str = super()._deserialize(*args, **kwargs)
-        uri = urlparse(uri_str)
-
-        if uri.fragment:
-            raise PyBioValidationException(f"Invalid URI: {uri}. Got URI fragment: {uri.fragment}")
-        if uri.params:
-            raise PyBioValidationException(f"Invalid URI: {uri}. Got URI params: {uri.params}")
-
-        return raw_nodes.URI(scheme=uri.scheme, netloc=uri.netloc, path=uri.path, query=uri.query)
+#########################
+# more specialized fields
+#########################
 
 
-class Path(String):
-    def _deserialize(self, *args, **kwargs):
-        path_str = super()._deserialize(*args, **kwargs)
-        return pathlib.Path(path_str)
+class Axes(String):
+    def _deserialize(self, *args, **kwargs) -> str:
+        axes_str = super()._deserialize(*args, **kwargs)
+        valid_axes = self.metadata.get("valid_axes", "bczyx")
+        if any(a not in valid_axes for a in axes_str):
+            raise PyBioValidationException(f"Invalid axes! Valid axes consist of: {valid_axes}")
+
+        return axes_str
 
 
-class SHA256(String):
-    def _deserialize(self, *args, **kwargs):
-        value_str = super()._deserialize(*args, **kwargs)
-        return value_str
+class Dependencies(String):  # todo: check format of dependency string
+    pass
+
+
+class ExplicitShape(List):
+    def __init__(self, **super_kwargs):
+        super().__init__(Integer, **super_kwargs)
+
+
+class Halo(List):
+    def __init__(self, *super_args, **super_kwargs):
+        super().__init__(Integer, *super_args, **super_kwargs)
+
+    def _deserialize(self, value, attr, data, **kwargs) -> typing.List[typing.Any]:
+        if value is None:
+            return [0] * len(data["shape"])
+        else:
+            return super()._deserialize(value, attr, data, **kwargs)
 
 
 class ImportableSource(String):
@@ -207,19 +218,50 @@ class ImportableSource(String):
             raise ValidationError(source_str)
 
 
-# class Kwargs(Dict):
-#     def _deserialize(self, value, attr, data, **kwargs):
-#         return nodes.Kwargs(**value)
+class InputShape(Union):
+    def __init__(self, **super_kwargs):
+        from pybio.spec.schema import ImplicitInputShape
+
+        super().__init__(
+            fields=[
+                ExplicitShape(
+                    bioimageio_description="Exact shape with same length as `axes`, e.g. `shape: [1, 512, 512, 1]`"
+                ),
+                Nested(
+                    ImplicitInputShape,
+                    bioimageio_description="A sequence of valid shapes given by `shape = min + k * step for k in {0, 1, ...}`.",
+                ),
+            ],
+            **super_kwargs,
+        )
 
 
-class Axes(String):
-    def _deserialize(self, *args, **kwargs) -> str:
-        axes_str = super()._deserialize(*args, **kwargs)
-        valid_axes = self.metadata.get("valid_axes", "bczyx")
-        if any(a not in valid_axes for a in axes_str):
-            raise PyBioValidationException(f"Invalid axes! Valid axes consist of: {valid_axes}")
+class Kwargs(Dict):
+    def __init__(self, keys=String, missing=dict, bioimageio_description="Key word arguments.", **super_kwargs):
+        super().__init__(keys, missing=missing, bioimageio_description=bioimageio_description, **super_kwargs)
 
-        return axes_str
+
+class OutputShape(Union):
+    def __init__(self, **super_kwargs):
+        from pybio.spec.schema import ImplicitOutputShape
+
+        super().__init__(
+            fields=[
+                ExplicitShape(),
+                Nested(
+                    ImplicitOutputShape,
+                    bioimageio_description="In reference to the shape of an input tensor, the shape of the output "
+                    "tensor is `shape = shape(input_tensor) * scale + 2 * offset`.",
+                ),
+            ],
+            **super_kwargs,
+        )
+
+
+class Path(String):
+    def _deserialize(self, *args, **kwargs):
+        path_str = super()._deserialize(*args, **kwargs)
+        return pathlib.Path(path_str)
 
 
 class ProcMode(String):
@@ -251,95 +293,49 @@ class ProcMode(String):
         super().__init__(validate=validate, required=required, **kwargs)
 
 
-class Dependencies(String):  # todo: check format of dependency string
-    pass
+class SHA256(String):
+    def _deserialize(self, *args, **kwargs):
+        value_str = super()._deserialize(*args, **kwargs)
+        return value_str
 
 
-class ExplicitShape(List):
-    def __init__(self, **super_kwargs):
-        super().__init__(Integer, **super_kwargs)
+class SpecURI(Nested):
+    def _deserialize(self, value, attr, data, **kwargs):
+        uri = urlparse(value)
+
+        if uri.query:
+            raise PyBioValidationException(f"Invalid URI: {uri}. Got URI query: {uri.query}")
+        if uri.fragment:
+            raise PyBioValidationException(f"Invalid URI: {uri}. Got URI fragment: {uri.fragment}")
+        if uri.params:
+            raise PyBioValidationException(f"Invalid URI: {uri}. Got URI params: {uri.params}")
+
+        # account for leading '/' for windows paths, e.g. '/C:/folder'
+        # see https://stackoverflow.com/questions/43911052/urlparse-on-a-windows-file-scheme-uri-leaves-extra-slash-at-start
+        path = url2pathname(uri.path)
+
+        return raw_nodes.SpecURI(spec_schema=self.schema, scheme=uri.scheme, netloc=uri.netloc, path=path, query="")
 
 
-class InputShape(Union):
-    def __init__(self, **super_kwargs):
-        from pybio.spec.schema import ImplicitInputShape
-
-        super().__init__(
-            fields=[
-                ExplicitShape(
-                    bioimageio_description="Exact shape with same length as `axes`, e.g. `shape: [1, 512, 512, 1]`"
-                ),
-                Nested(
-                    ImplicitInputShape,
-                    bioimageio_description="A sequence of valid shapes given by `shape = min + k * step for k in {0, 1, ...}`.",
-                ),
-            ],
-            **super_kwargs,
-        )
+class StrictVersion(String):
+    def _deserialize(
+        self,
+        value: typing.Any,
+        attr: typing.Optional[str],
+        data: typing.Optional[typing.Mapping[str, typing.Any]],
+        **kwargs,
+    ):
+        return distutils.version.StrictVersion(str(value))
 
 
-class OutputShape(Union):
-    def __init__(self, **super_kwargs):
-        from pybio.spec.schema import ImplicitOutputShape
+class URI(String):
+    def _deserialize(self, *args, **kwargs) -> raw_nodes.URI:
+        uri_str = super()._deserialize(*args, **kwargs)
+        uri = urlparse(uri_str)
 
-        super().__init__(
-            fields=[
-                ExplicitShape(),
-                Nested(
-                    ImplicitOutputShape,
-                    bioimageio_description="In reference to the shape of an input tensor, the shape of the output "
-                    "tensor is `shape = shape(input_tensor) * scale + 2 * offset`.",
-                ),
-            ],
-            **super_kwargs,
-        )
+        if uri.fragment:
+            raise PyBioValidationException(f"Invalid URI: {uri}. Got URI fragment: {uri.fragment}")
+        if uri.params:
+            raise PyBioValidationException(f"Invalid URI: {uri}. Got URI params: {uri.params}")
 
-
-class Array(marshmallow_fields.Field):
-    def __init__(self, inner: marshmallow_fields.Field, **kwargs):
-        self.inner = inner
-        super().__init__(**kwargs)
-
-    @property
-    def dtype(self) -> typing.Union[typing.Type[int], typing.Type[float], typing.Type[str]]:
-        if isinstance(self.inner, Integer):
-            return int
-        elif isinstance(self.inner, Float):
-            return float
-        elif isinstance(self.inner, String):
-            return str
-        else:
-            raise NotImplementedError(self.inner)
-
-    def _deserialize_inner(self, value):
-        if isinstance(value, list):
-            return [self._deserialize_inner(v) for v in value]
-        else:
-            return self.inner.deserialize(value)
-
-    def deserialize(self, value: typing.Any, attr: str = None, data: typing.Mapping[str, typing.Any] = None, **kwargs):
-        value = self._deserialize_inner(value)
-
-        if isinstance(value, list):
-            try:
-                return numpy.array(value, dtype=self.dtype)
-            except ValueError as e:
-                raise PyBioValidationException(str(e)) from e
-        else:
-            return value
-
-
-class Halo(List):
-    def __init__(self, *super_args, **super_kwargs):
-        super().__init__(Integer, *super_args, **super_kwargs)
-
-    def _deserialize(self, value, attr, data, **kwargs) -> typing.List[typing.Any]:
-        if value is None:
-            return [0] * len(data["shape"])
-        else:
-            return super()._deserialize(value, attr, data, **kwargs)
-
-
-class Kwargs(Dict):
-    def __init__(self, keys=String, missing=dict, bioimageio_description="Key word arguments.", **super_kwargs):
-        super().__init__(keys, missing=missing, bioimageio_description=bioimageio_description, **super_kwargs)
+        return raw_nodes.URI(scheme=uri.scheme, netloc=uri.netloc, path=uri.path, query=uri.query)
