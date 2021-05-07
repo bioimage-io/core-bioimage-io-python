3740645eee60ee1fd9aa7f65163acdde84f6f519
fynnbe <thefynnbe@gmail.com>
2021-05-07 09:37:32 +0200

    update maybe_convert_to_v0_3  to 0.3.1

--- pybio/spec/utils/maybe_convert_to_v0_3.py
+++ pybio/spec/utils/maybe_convert_to_v0_3.py
@@ -23,7 +23,7 @@ def maybe_convert_to_v0_3(data: Dict) -> Dict:
     schema_v0_1.Model().validate(data)
 
     data = copy.deepcopy(data)
-    data["format_version"] = "0.3.0"
+    data["format_version"] = "0.3.1"
 
     data["kwargs"] = {k: None for k in data.pop("required_kwargs", set())}
     data["kwargs"].update(data.pop("optional_kwargs", {}))
@@ -45,6 +45,11 @@ def maybe_convert_to_v0_3(data: Dict) -> Dict:
     except KeyError:
         conversion_errors["config"]["future"]["0.3.0"] = missing
         future = {}
+
+    try:
+        future.update(data["config"]["future"].pop("0.3.1"))
+    except KeyError:
+        conversion_errors["config"]["future"]["0.3.1"] = missing
 
     try:
         data["git_repo"] = future.pop("git_repo")
@@ -123,7 +128,8 @@ def maybe_convert_to_v0_3(data: Dict) -> Dict:
     del data["prediction"]
     del data["training"]
     # remove 'future' from config if no other than the used future entries exist
-    if not data.get("config", {}).get("future"):
-        del data["config"]["future"]
+    config = data.get("config", {})
+    if config.get("future") == {}:
+        del config["future"]
 
     return data
