2d5734ccdddfe02093424bc1d677f18253f4cd4b
fynnbe <thefynnbe@gmail.com>
2021-05-28 17:18:45 +0200

    remove spec module

--- bioimageio/spec/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .build_spec import build_spec
-from .common import yaml
-from .maybe_convert import maybe_convert
-from .transformers import resolve_uri, load_model_spec, load_spec, get_instance
