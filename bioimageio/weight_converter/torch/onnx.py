89af221eac08afe6433a2a2b33911653dd3f6682
Constantin Pape <c.pape@gmx.net>
2021-06-15 14:07:04 +0200

    Add weight converters WIP

---
+++ bioimageio/weight_converter/torch/onnx.py
@@ -0,0 +1,83 @@
+import argparse
+import os
+import sys
+import warnings
+from pathlib import Path
+from typing import Union
+
+import onnxruntime as rt
+import numpy as np
+import torch
+from numpy.testing import assert_array_almost_equal
+
+from pybio.spec.utils.transformers import load_and_resolve_spec
+from pybio.spec.utils import get_instance
+
+
+def convert_weights_to_onnx(
+    model_yaml: Union[str, Path],
+    output_path: Union[str, Path],
+    opset_version: Union[str, None] = 12,
+    use_tracing: bool = True,
+    verbose: bool = True
+):
+    """ Convert model weights from format 'pytorch_state_dict' to 'onnx'.
+
+    Arguments:
+        model_yaml: location of the model.yaml file with bioimage.io spec
+        output_path: where to save the onnx weights
+        opset_version: onnx opset version
+        use_tracing: whether to use tracing or scripting to export the onnx format
+        verbose: be verbose during the onnx export
+    """
+    spec = load_and_resolve_spec(model_yaml)
+
+    with torch.no_grad():
+        # load input and expected output data
+        input_data = np.load(spec.test_inputs[0]).astype('float32')
+        input_tensor = torch.from_numpy(input_data)
+
+        # instantiate and generate the expected output
+        model = get_instance(spec)
+        state = torch.load(spec.weights['pytorch_state_dict'].source)
+        model.load_state_dict(state)
+        expected_output = model(input_tensor).numpy()
+
+        if use_tracing:
+            torch.onnx.export(model, input_tensor, output_path, verbose=verbose,
+                              opset_version=opset_version)
+        else:
+            raise NotImplementedError
+
+        # check the onnx model
+        sess = rt.InferenceSession(output_path)
+        input_name = sess.get_inputs()[0].name
+        output = sess.run(None, {input_name: input_data})[0]
+
+        try:
+            assert_array_almost_equal(expected_output, output, decimal=4)
+            return 0
+        except AssertionError as e:
+            msg = f"The onnx weights were exported, but results before and after conversion do not agree:\n {str(e)}"
+            warnings.warn(msg)
+            return 1
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", "-m", required=True)
+    parser.add_argument("--output", "-o", required=True)
+    parser.add_argument("--opset_version", default=12, type=int)
+    parser.add_argument("--tracing", "-t", default=1, type=int)
+    parser.add_argument("--verbose", "-v", default=1, type=int)
+
+    args = parser.parse_args()
+    return convert_weights_to_onnx(os.path.abspath(args.model),
+                                   args.output,
+                                   args.opset_version,
+                                   bool(args.tracing),
+                                   bool(args.verbose))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ bioimageio/weight_converter/torch/torchscript.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+import sys
+import warnings
+
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+import torch
+from numpy.testing import assert_array_almost_equal
+
+from pybio.spec.utils.transformers import load_and_resolve_spec
+from pybio.spec.utils import get_instance
+
+
+def convert_weights_to_torchscript(
+    model_yaml: Union[str, Path],
+    output_path: Union[str, Path],
+    use_tracing: bool = True
+):
+    """ Convert model weights from format 'pytorch_state_dict' to 'torchscript'.
+    """
+    spec = load_and_resolve_spec(model_yaml)
+
+    with torch.no_grad():
+        # load input and expected output data
+        input_data = np.load(spec.test_inputs[0]).astype('float32')
+        input_data = torch.from_numpy(input_data)
+
+        # instantiate model and get reference output
+        model = get_instance(spec)
+        state = torch.load(spec.weights['pytorch_state_dict'].source)
+        model.load_state_dict(state)
+
+        # get the expected output to validate the torchscript weights
+        expected_output = model(input_data)
+
+        # make scripted model
+        if use_tracing:
+            scripted_model = torch.jit.trace(model, input_data)
+        else:
+            scripted_model = torch.jit.script(model)
+
+        # check the scripted model
+        output = scripted_model(input_data).numpy()
+
+    # save the torchscript model
+    scripted_model.save(output_path)
+
+    try:
+        assert_array_almost_equal(expected_output, output, decimal=4)
+        return 0
+    except AssertionError as e:
+        msg = f"The onnx weights were exported, but results before and after conversion do not agree:\n {str(e)}"
+        warnings.warn(msg)
+        return 1
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", "-m", required=True)
+    parser.add_argument("--output", "-o", required=True)
+    parser.add_argument("--tracing", "-t", default=1, type=int)
+
+    args = parser.parse_args()
+    return convert_weights_to_torchscript(os.path.abspath(args.model),
+                                          args.output,
+                                          bool(args.tracing))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
