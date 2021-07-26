e1f243d5fc1b9533fbb7118079e6d24ded3ba22f
Constantin Pape <c.pape@gmx.net>
2021-07-26 10:42:20 +0200

    Implement tests for weight_converter.torch WIP

---
+++ tests/weight_converter/torch/test_torchscript.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from bioimageio.spec import export_package
+
+
+# TODO only run this test if we have pytorch
+def test_torchscript_converter(unet2d_nuclei_broad_model_url):
+    spec_path = export_package(unet2d_nuclei_broad_model_url, Path("./data"))
+
