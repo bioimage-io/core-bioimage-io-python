3d3d7d65ddc36837015fe9063fe9d1d719ee9456
fynnbe <thefynnbe@gmail.com>
2021-07-27 20:14:02 +0200

    remove wrong root_path

--- tests/weight_converter/torch/test_torchscript.py
+++ tests/weight_converter/torch/test_torchscript.py
@@ -1,8 +1,7 @@
-from pathlib import Path
 from bioimageio.spec import export_package
 
 
 # TODO only run this test if we have pytorch
 def test_torchscript_converter(unet2d_nuclei_broad_model_url):
-    spec_path = export_package(unet2d_nuclei_broad_model_url, Path("./data"))
+    spec_path = export_package(unet2d_nuclei_broad_model_url)
 
