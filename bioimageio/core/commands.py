750bafd858a0388658d35dac2f194a3cc3fa7e1c
fynnbe <thefynnbe@gmail.com>
2021-09-07 19:44:01 +0200

    update package commands

--- bioimageio/core/commands.py
+++ bioimageio/core/commands.py
@@ -1,34 +1,29 @@
 import shutil
 import traceback
 from pathlib import Path
-from pprint import pprint
 from typing import List, Optional, Union
 
-from marshmallow import ValidationError
-
-from bioimageio.spec import export_resource_package, load_raw_resource_description
-from bioimageio.spec.shared.raw_nodes import URI
+from bioimageio.core import export_resource_package
 from bioimageio.core.resource_io.utils import resolve_uri
+from bioimageio.spec.commands import validate
+from bioimageio.spec.shared.raw_nodes import URI
 
 
 def package(
     rdf_source: Union[Path, str, URI, dict],
     path: Path = Path() / "{src_name}-package.zip",
-    update_format: bool = False,
     weights_priority_order: Optional[List[str]] = None,
     verbose: bool = False,
 ) -> int:
     """Package a BioImage.IO resource described by a BioImage.IO Resource Description File (RDF)."""
-    code = validate(rdf_source, update_format=update_format, update_format_inner=update_format, verbose=verbose)
+    code = validate(rdf_source, update_format=True, update_format_inner=True, verbose=verbose)
     source_name = rdf_source.get("name") if isinstance(rdf_source, dict) else rdf_source
     if code:
-        print(f"Cannot export invalid BioImage.IO RDF {source_name}")
+        print(f"Cannot package invalid BioImage.IO RDF {source_name}")
         return code
 
     try:
-        tmp_package_path = export_resource_package(
-            rdf_source, update_to_current_format=update_format, weights_priority_order=weights_priority_order
-        )
+        tmp_package_path = export_resource_package(rdf_source, weights_priority_order=weights_priority_order)
     except Exception as e:
         print(f"Failed to package {source_name} due to: {e}")
         if verbose:
