04d05fcbf2427c31d177728a3da0a23e44929917
fynnbe <thefynnbe@gmail.com>
2021-09-07 13:17:56 +0200

    remove duplicated tests

--- tests/resource_io/test_load_rdf.py
+++ tests/resource_io/test_load_rdf.py
@@ -44,21 +44,7 @@ def test_load_raw_model(unet2d_nuclei_broad_model):
     assert raw_model
 
 
-def test_load_raw_model_from_package(unet2d_nuclei_broad_model):
-    from bioimageio.core import load_raw_resource_description
-
-    raw_model = load_raw_resource_description(unet2d_nuclei_broad_model)
-    assert raw_model
-
-
 def test_load_model(unet2d_nuclei_broad_model):
-    from bioimageio.core import load_resource_description
-
-    model = load_resource_description(unet2d_nuclei_broad_model)
-    assert model
-
-
-def test_load_model_from_package(unet2d_nuclei_broad_model):
     from bioimageio.core import load_resource_description
 
     model = load_resource_description(unet2d_nuclei_broad_model)
