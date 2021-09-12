9693ab8cdc2ae66524d46bf078555a92dd7744ca
fynnbe <thefynnbe@gmail.com>
2021-09-12 17:35:44 +0200

    multi tensor io in torchscript model adapater

--- bioimageio/core/prediction_pipeline/_model_adapters/_torchscript_model_adapter.py
+++ bioimageio/core/prediction_pipeline/_model_adapters/_torchscript_model_adapter.py
@@ -10,30 +10,25 @@ from ._model_adapter import ModelAdapter
 
 class TorchscriptModelAdapter(ModelAdapter):
     def __init__(self, *, bioimageio_model: nodes.Model, devices: Optional[List[str]] = None):
-        spec = bioimageio_model
-        self.name = spec.name
-
-        _input = spec.inputs[0]
-        _output = spec.outputs[0]
-
-        self._internal_input_axes = _input.axes
-        self._internal_output_axes = _output.axes
-
+        weight_path = str(bioimageio_model.weights["pytorch_script"].source.resolve())
         if devices is None:
-            self.devices = ["cuda" if torch.cuda.is_available() else "cpu"]
+            devices = ["cuda" if torch.cuda.is_available() else "cpu"]
         else:
-            self.devices = [torch.device(d) for d in devices]
+            devices = [torch.device(d) for d in devices]
 
-        weight_path = str(spec.weights["pytorch_script"].source.resolve())
-        self.model = torch.jit.load(weight_path)
-        self.model.to(self.devices[0])
+        self._model = torch.jit.load(weight_path)
+        self._model.to(devices[0])
+        self._internal_output_axes = [tuple(out.axes) for out in bioimageio_model.outputs]
 
-    def forward(self, batch: xr.DataArray) -> xr.DataArray:
+    def forward(self, *batch: xr.DataArray) -> List[xr.DataArray]:
         with torch.no_grad():
-            torch_tensor = torch.from_numpy(batch.data)
-            result = self.model.forward(torch_tensor)
+            torch_tensor = [torch.from_numpy(b.data) for b in batch]
+            result = self._model.forward(*torch_tensor)
+            if not isinstance(result, (tuple, list)):
+                result = [result]
 
-            if not isinstance(result, np.ndarray):
-                result = result.cpu().numpy()
+            result = [r.cpu().numpy() if not isinstance(r, np.ndarray) else r for r in result]
 
-        return xr.DataArray(result, dims=tuple(self._internal_output_axes))
+        assert len(result) == len(self._internal_output_axes)
+        result = [xr.DataArray(r, dims=axes) for r, axes in zip(result, self._internal_output_axes)]
+        return result
