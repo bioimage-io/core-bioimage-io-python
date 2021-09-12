b51c1f960aa6d4df057d4902e97715db3a38de13
fynnbe <thefynnbe@gmail.com>
2021-09-12 18:52:52 +0200

    multi tensor io in onnx model adapter

--- bioimageio/core/prediction_pipeline/_model_adapters/_onnx_model_adapter.py
+++ bioimageio/core/prediction_pipeline/_model_adapters/_onnx_model_adapter.py
@@ -14,28 +14,20 @@ logger = logging.getLogger(__name__)
 class ONNXModelAdapter(ModelAdapter):
     def __init__(self, *, bioimageio_model: nodes.Model, devices: Optional[List[str]] = None):
         spec = bioimageio_model
-        self.name = spec.name
 
-        if len(spec.inputs) != 1 or len(spec.outputs) != 1:
-            raise NotImplementedError("Only single input, single output models are supported")
-
-        assert len(spec.inputs) == 1
-        assert len(spec.outputs) == 1
-
-        spec.inputs[0]
-        _output = spec.outputs[0]
-
-        self._internal_output_axes = _output.axes
+        self._internal_output_axes = [tuple(out.axes) for out in bioimageio_model.outputs]
 
         self._session = rt.InferenceSession(str(spec.weights["onnx"].source))
         onnx_inputs = self._session.get_inputs()
-        assert len(onnx_inputs) == 1, f"expected onnx model to have one input got {len(onnx_inputs)}"
-        self._input_name = onnx_inputs[0].name
-        # TODO onnx device management
-        self.devices = []
+        self._input_names = [ipt.name for ipt in onnx_inputs]
+
         if devices is not None:
             warnings.warn(f"Device management is not implemented for onnx yet, ignoring the devices {devices}")
 
-    def forward(self, input: xr.DataArray) -> xr.DataArray:
-        result = self._session.run(None, {self._input_name: input.data})[0]
-        return xr.DataArray(result, dims=tuple(self._internal_output_axes))
+    def forward(self, *input_tensors: xr.DataArray) -> List[xr.DataArray]:
+        assert len(input_tensors) == len(self._input_names)
+        result = self._session.run(None, dict(zip(self._input_names, input_tensors)))
+        if not isinstance(result, (list, tuple)):
+            result = []
+
+        return [xr.DataArray(r, dims=axes) for r, axes in zip(result, self._internal_output_axes)]
