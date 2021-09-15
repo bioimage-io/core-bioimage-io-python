80bf0e4107b85df4b1a93210f06208fba00c66aa
fynnbe <thefynnbe@gmail.com>
2021-09-15 19:26:13 +0200

    don't test onnx converter with 'any_torch_model' yet

--- tests/weight_converter/torch/test_onnx.py
+++ tests/weight_converter/torch/test_onnx.py
@@ -2,11 +2,12 @@ import os
 import pytest
 
 
-def test_onnx_converter_from_torch(any_torch_model, tmp_path):
+# todo: test with 'any_torch_model'
+def test_onnx_converter_from_torch(unet2d_nuclei_broad, tmp_path):
     from bioimageio.core.weight_converter.torch.onnx import convert_weights_to_onnx
 
     out_path = tmp_path / "weights.onnx"
-    ret_val = convert_weights_to_onnx(any_torch_model, out_path, test_decimal=3)
+    ret_val = convert_weights_to_onnx(unet2d_nuclei_broad, out_path, test_decimal=3)
     assert os.path.exists(out_path)
     if not pytest.skip_onnx:
         assert ret_val == 0  # check for correctness is done in converter and returns 0 if it passes
