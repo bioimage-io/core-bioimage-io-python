03f4b70ec3949964334f541292d31be0b496f4b9
Constantin Pape <c.pape@gmx.net>
2021-09-17 12:16:31 +0200

    Ensure output dtype in post-processing

---
+++ tests/prediction_pipeline/test_postprocessing.py
@@ -0,0 +1,20 @@
+import numpy as np
+import xarray as xr
+from bioimageio.core.resource_io.nodes import Postprocessing
+from bioimageio.core.prediction_pipeline._postprocessing import make_postprocessing
+
+
+def test_binarize_postprocessing():
+    shape = (3, 32, 32)
+    axes = ("c", "y", "x")
+    np_data = np.random.rand(*shape)
+    data = xr.DataArray(np_data, dims=axes)
+
+    threshold = 0.5
+    exp = xr.DataArray(np_data > threshold, dims=axes)
+
+    for dtype in ("float32", "float64", "uint8", "uint16"):
+        binarize = make_postprocessing(spec=[Postprocessing("binarize", kwargs={"threshold": threshold})], dtype=dtype)
+        res = binarize(data)
+        assert np.dtype(res.dtype) == np.dtype(dtype)
+        xr.testing.assert_allclose(res, exp.astype(dtype))
