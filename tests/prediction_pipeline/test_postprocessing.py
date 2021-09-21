20359383233faa773c8e83d8791576ee0d9b2923
fynnbe <thefynnbe@gmail.com>
2021-09-21 13:22:10 +0200

    really update postprocessing

--- tests/prediction_pipeline/test_postprocessing.py
+++ tests/prediction_pipeline/test_postprocessing.py
@@ -1,10 +1,10 @@
 import numpy as np
 import xarray as xr
-from bioimageio.core.resource_io.nodes import Postprocessing
-from bioimageio.core.prediction_pipeline._combined_processing import make_postprocessing
 
 
 def test_binarize_postprocessing():
+    from bioimageio.core.prediction_pipeline._processing import Binarize
+
     shape = (3, 32, 32)
     axes = ("c", "y", "x")
     np_data = np.random.rand(*shape)
@@ -13,8 +13,6 @@ def test_binarize_postprocessing():
     threshold = 0.5
     exp = xr.DataArray(np_data > threshold, dims=axes)
 
-    for dtype in ("float32", "float64", "uint8", "uint16"):
-        binarize = make_postprocessing(spec=[Postprocessing("binarize", kwargs={"threshold": threshold})], dtype=dtype)
-        res = binarize(data)
-        assert np.dtype(res.dtype) == np.dtype(dtype)
-        xr.testing.assert_allclose(res, exp.astype(dtype))
+    binarize = Binarize("data_name", threshold=threshold)
+    res = binarize(data)
+    xr.testing.assert_allclose(res, exp)
