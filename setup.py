c68fd7700d659596918a4d9f567a38fefb11dad6
fynnbe <thefynnbe@gmail.com>
2021-10-20 11:27:48 +0200

    add tqdm

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         "Programming Language :: Python :: 3.8",
     ],
     packages=find_namespace_packages(exclude=["tests"]),  # Required
-    install_requires=["bioimageio.spec", "imageio>=2.5", "numpy", "ruamel.yaml", "xarray"],
+    install_requires=["bioimageio.spec", "imageio>=2.5", "numpy", "ruamel.yaml", "tqdm", "xarray"],
     include_package_data=True,
     extras_require={
         "test": ["pytest"],
