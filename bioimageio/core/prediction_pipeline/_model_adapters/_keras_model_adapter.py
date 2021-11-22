54062fe49ecbe78b34cf89ee0abbbfdc9df0f284
fynnbe <thefynnbe@gmail.com>
2021-11-22 18:50:12 +0100

    update KerasModelAdapter

--- bioimageio/core/prediction_pipeline/_model_adapters/_keras_model_adapter.py
+++ bioimageio/core/prediction_pipeline/_model_adapters/_keras_model_adapter.py
@@ -4,19 +4,18 @@ from typing import List, Optional
 import keras
 import xarray as xr
 
-from bioimageio.core.resource_io import nodes
 from ._model_adapter import ModelAdapter
 
 
 class KerasModelAdapter(ModelAdapter):
-    def __init__(self, *, bioimageio_model: nodes.Model, devices: Optional[List[str]] = None):
+    def _load(self, *, devices: Optional[Sequence[str]] = None) -> None:
         # TODO keras device management
         if devices is not None:
             warnings.warn(f"Device management is not implemented for tensorflow yet, ignoring the devices {devices}")
 
-        weight_file = bioimageio_model.weights["keras_hdf5"].source
+        weight_file = self.bioimageio_model.weights["keras_hdf5"].source
         self._model = keras.models.load_model(weight_file)
-        self._output_axes = [tuple(out.axes) for out in bioimageio_model.outputs]
+        self._output_axes = [tuple(out.axes) for out in self.bioimageio_model.outputs]
 
     def forward(self, *input_tensors: xr.DataArray) -> List[xr.DataArray]:
         result = self._model.predict(*input_tensors)
