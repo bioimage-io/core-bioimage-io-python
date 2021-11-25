92c8bfcae146fc9c11fbb1252df610f7c625d1b3
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2021-11-25 11:56:18 +0100

    Update tests/test_resource_tests/test_test_model.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/test_resource_tests/test_test_model.py
+++ tests/test_resource_tests/test_test_model.py
@@ -2,9 +2,11 @@ def test_error_for_wrong_shape(stardist_wrong_shape):
     from bioimageio.core.resource_tests import test_model
 
     summary = test_model(stardist_wrong_shape)
-    expected_error_message = ("Shape of test output 0 'output' does not match output shape description: "
-                              "ImplicitOutputShape(reference_tensor='input', "
-                              "scale=[1.0, 1.0, 1.0, 0.0], offset=[1.0, 1.0, 1.0, 33.0]).")
+    expected_error_message = (
+        "Shape of test output 0 'output' does not match output shape description: "
+        "ImplicitOutputShape(reference_tensor='input', "
+        "scale=[1.0, 1.0, 1.0, 0.0], offset=[1.0, 1.0, 1.0, 33.0])."
+    )
     assert summary["error"] == expected_error_message
 
 
