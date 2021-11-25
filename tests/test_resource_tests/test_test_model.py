5926bab6b306b2802ebba06b1e93f7b3f6354b23
Constantin Pape <c.pape@gmx.net>
2021-11-25 13:58:42 +0100

    Add another test to ensure wrong shapes are caught by test_model

--- tests/test_resource_tests/test_test_model.py
+++ tests/test_resource_tests/test_test_model.py
@@ -10,6 +10,17 @@ def test_error_for_wrong_shape(stardist_wrong_shape):
     assert summary["error"] == expected_error_message
 
 
+def test_error_for_wrong_shape2(stardist_wrong_shape2):
+    from bioimageio.core.resource_tests import test_model
+
+    summary = test_model(stardist_wrong_shape2)
+    expected_error_message = (
+        "Shape (1, 512, 512, 1) of test input 0 'input' does not match input shape description: "
+        "ParametrizedInputShape(min=[1, 16, 16, 1], step=[0, 17, 17, 0])."
+    )
+    assert summary["error"] == expected_error_message
+
+
 def test_test_model(any_model):
     from bioimageio.core.resource_tests import test_model
 
