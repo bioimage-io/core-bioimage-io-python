4a41c1120f5ffc56c73de7c5995dd80a782828f0
fynnbe <thefynnbe@gmail.com>
2022-03-08 12:40:19 +0100

    skip test_load_remote_rdf if torch is not available

--- tests/resource_io/test_load_rdf.py
+++ tests/resource_io/test_load_rdf.py
@@ -70,6 +70,7 @@ def test_load_model_with_rel_str_source(unet2d_nuclei_broad_model):
     assert model
 
 
+@pytest.mark.skipif(pytest.skip_torch, reason="remote model is a pytorch model")
 def test_load_remote_rdf():
     from bioimageio.core import load_resource_description
     from bioimageio.core.resource_io import nodes
