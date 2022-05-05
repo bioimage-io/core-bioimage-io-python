33e49f8d81abc086319e9d64cfa85e9d795a6bd0
fynnbe <thefynnbe@gmail.com>
2022-05-05 16:43:23 +0200

    WIP add test_output_shape_with_offset

--- tests/test_resource_tests/test_test_model.py
+++ tests/test_resource_tests/test_test_model.py
@@ -1,3 +1,6 @@
+from bioimageio.spec.shared import resolve_rdf_source, yaml
+
+
 def test_error_for_wrong_shape(stardist_wrong_shape):
     from bioimageio.core.resource_tests import test_model
 
@@ -33,3 +36,26 @@ def test_test_resource(any_model):
 
     summary = test_resource(any_model)
     assert summary["error"] is None
+
+
+def test_output_shape_with_offset():
+    from bioimageio.core.resource_tests import test_resource
+
+    url = "https://zenodo.org/record/6383430/files/rdf.yaml"
+    data, name, root = resolve_rdf_source(url)
+    data["root_path"] = root
+    data["outputs"] = yaml.load(
+        """
+- axes: bczyx
+  data_range: [0, 1]
+  data_type: float32
+  halo: [0, 0, 8, 16, 16]
+  name: output0
+  shape:
+    offset: [0, 1, 0, 0, 0]  # fixed channel diff
+    reference_tensor: raw
+    scale: [1, 1, 1, 1, 1] 
+    """
+    )
+    summary = test_resource(data)
+    assert summary["status"] == "passed", summary
