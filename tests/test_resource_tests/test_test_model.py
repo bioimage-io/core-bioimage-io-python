91d77c72a6117bd8e12839b4fd9678b9b6351b82
fynnbe <thefynnbe@gmail.com>
2022-05-11 17:21:15 +0200

    add test_validation_section_warning

--- tests/test_resource_tests/test_test_model.py
+++ tests/test_resource_tests/test_test_model.py
@@ -1,3 +1,8 @@
+import pathlib
+
+from bioimageio.spec.shared import yaml
+
+
 def test_error_for_wrong_shape(stardist_wrong_shape):
     from bioimageio.core.resource_tests import test_model
 
@@ -33,3 +38,23 @@ def test_test_resource(any_model):
 
     summary = test_resource(any_model)[0]
     assert summary["error"] is None
+
+
+def test_validation_section_warning(unet2d_nuclei_broad_model, tmp_path: pathlib.Path):
+    from bioimageio.core.resource_tests import test_resource
+    from bioimageio.core import load_resource_description
+
+    model = load_resource_description(unet2d_nuclei_broad_model)
+
+    summary = test_resource(model)[1]
+    assert summary["name"] == "Test documentation completeness."
+    assert summary["warnings"] == {"documentation": "No '# Validation' (sub)section found."}
+    assert summary["status"] == "passed"
+
+    doc_with_validation = tmp_path / "doc.md"
+    doc_with_validation.write_text("# Validation\nThis is a section about how to validate the model on new data")
+    model.documentation = doc_with_validation
+    summary = test_resource(model)[1]
+    assert summary["name"] == "Test documentation completeness."
+    assert summary["warnings"] == {}
+    assert summary["status"] == "passed"
