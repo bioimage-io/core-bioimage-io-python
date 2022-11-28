43f989990044152da9089866f35e5ddfa70ee95d
fynnbe <thefynnbe@gmail.com>
2022-11-28 14:45:47 +0100

    remove contrib module

--- bioimageio/core/contrib/contrib_a/local.py
+++
@@ -1,2 +0,0 @@
-from ._demo import hello
-from ._inference import run_model_inference_with_dask
--- bioimageio/core/contrib/contrib_a/remote.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pathlib import Path
-
-from bioimageio.core.contrib.utils import RemoteContrib
-
-remote_module = RemoteContrib(Path(__file__).parent.stem)
-__all__ = remote_module.__all__
-sys.modules[__name__] = remote_module  # noqa
