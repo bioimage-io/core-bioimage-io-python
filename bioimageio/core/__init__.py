e2b4b713e679acbfb20d25c65bc6b9be19c1e8f8
fynnbe <thefynnbe@gmail.com>
2023-08-16 14:10:54 +0200

    specify __all__

--- bioimageio/core/__init__.py
+++ bioimageio/core/__init__.py
@@ -3,6 +3,13 @@ import pathlib
 
 __version__ = json.loads((pathlib.Path(__file__).parent / "VERSION").read_text())["version"]
 
+from .prediction import (
+    predict_image,
+    predict_images,
+    predict_with_padding,
+    predict_with_tiling,
+)
+from .prediction_pipeline import create_prediction_pipeline
 from .resource_io import (
     export_resource_package,
     load_raw_resource_description,
@@ -10,6 +17,21 @@ from .resource_io import (
     save_raw_resource_description,
     serialize_raw_resource_description,
 )
-from .prediction_pipeline import create_prediction_pipeline
-from .prediction import predict_image, predict_images, predict_with_padding, predict_with_tiling
 from .resource_tests import check_input_shape, check_output_shape, test_resource
+
+__all__ = [
+    "__version__",
+    "check_input_shape",
+    "check_output_shape",
+    "create_prediction_pipeline",
+    "export_resource_package",
+    "load_raw_resource_description",
+    "load_resource_description",
+    "predict_image",
+    "predict_images",
+    "predict_with_padding",
+    "predict_with_tiling",
+    "save_raw_resource_description",
+    "serialize_raw_resource_description",
+    "test_resource",
+]
