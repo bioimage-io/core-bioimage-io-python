ca41439181984911b29e366fc8e8bfc691f4a2b9
fynnbe <thefynnbe@gmail.com>
2023-10-12 00:31:23 +0200

    update model adapters

--- bioimageio/core/model_adapters/_pytorch_model_adapter.py
+++ bioimageio/core/model_adapters/_pytorch_model_adapter.py
@@ -1,17 +1,19 @@
 import gc
 import warnings
-from typing import List, Optional
+from typing import Any, List, Optional, Sequence, Tuple, Union
 
 import torch
 import xarray as xr
-from marshmallow import missing
 
-from bioimageio.core.resource_io import nodes
+from bioimageio.core.utils import import_callable
+from bioimageio.spec.model import AnyModel
+from bioimageio.spec.model.v0_4 import PytorchStateDictWeights as PytorchStateDictWeights04
+
 from ._model_adapter import ModelAdapter
 
 
 class PytorchModelAdapter(ModelAdapter):
-    def _load(self, *, devices: Optional[List[str]] = None):
+    def _load(self, *, devices: Optional[Sequence[str]] = None):
         self._model = self.get_nn_instance(self.bioimageio_model)
 
         if devices is None:
@@ -27,17 +29,18 @@ class PytorchModelAdapter(ModelAdapter):
         assert isinstance(self._model, torch.nn.Module)
         weights = self.bioimageio_model.weights.get("pytorch_state_dict")
         if weights is not None and weights.source:
-            state = torch.load(weights.source, map_location=self._devices[0])
-            self._model.load_state_dict(state)
+            state: Any = torch.load(weights.source, map_location=self._devices[0])
+            _ = self._model.load_state_dict(state)
 
-        self._model.eval()
+        _ = self._model.eval()
         self._internal_output_axes = [tuple(out.axes) for out in self.bioimageio_model.outputs]
 
     def _forward(self, *input_tensors: xr.DataArray) -> List[xr.DataArray]:
+        assert self._devices is not None
         with torch.no_grad():
             tensors = [torch.from_numpy(ipt.data) for ipt in input_tensors]
             tensors = [t.to(self._devices[0]) for t in tensors]
-            result = self._model(*tensors)
+            result: Union[Tuple[Any, ...], List[Any], Any] = self._model(*tensors)
             if not isinstance(result, (tuple, list)):
                 result = [result]
 
@@ -48,15 +51,22 @@ class PytorchModelAdapter(ModelAdapter):
     def _unload(self) -> None:
         self._devices = None
         del self._model
-        gc.collect()  # deallocate memory
+        _ = gc.collect()  # deallocate memory
         torch.cuda.empty_cache()  # release reserved memory
 
     @staticmethod
-    def get_nn_instance(model_node: nodes.Model, **kwargs):
-        weight_spec = model_node.weights.get("pytorch_state_dict")
+    def get_nn_instance(model: AnyModel):
+        weight_spec = model.weights.pytorch_state_dict
         assert weight_spec is not None
-        assert isinstance(weight_spec.architecture, nodes.ImportedSource)
-        model_kwargs = weight_spec.kwargs
-        joined_kwargs = {} if model_kwargs is missing else dict(model_kwargs)
-        joined_kwargs.update(kwargs)
-        return weight_spec.architecture(**joined_kwargs)
+        arch = import_callable(
+            weight_spec.architecture,
+            sha256=weight_spec.architecture_sha256
+            if isinstance(weight_spec, PytorchStateDictWeights04)
+            else weight_spec.sha256,
+        )
+        model_kwargs = (
+            weight_spec.kwargs
+            if isinstance(weight_spec, PytorchStateDictWeights04)
+            else weight_spec.architecture.kwargs
+        )
+        return arch(**model_kwargs)
--- bioimageio/core/prediction_pipeline/_model_adapters/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._model_adapter import ModelAdapter, create_model_adapter, get_weight_formats
-
-__all__ = ["ModelAdapter", "create_model_adapter", "get_weight_formats"]
