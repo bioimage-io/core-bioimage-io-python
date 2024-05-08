13f215c8aab9a4fe124e891c7add0c896b5406ad
fynnbe <thefynnbe@gmail.com>
2024-05-08 12:38:20 +0200

    update docstring

--- bioimageio/core/prediction.py
+++ bioimageio/core/prediction.py
@@ -1,6 +1,6 @@
 """convenience functions for prediction coming soon.
 For now, please use `create_prediction_pipeline` to get a `PredictionPipeline`
-and then `PredictionPipeline.predict(sample)`
+and then `PredictionPipeline.predict_sample(sample)`
 e..g load samples with core.io.load_sample_for_model()
 """
 
