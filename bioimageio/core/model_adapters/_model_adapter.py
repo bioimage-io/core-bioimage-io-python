0dc1c630dc024d11d71d5e8b91b3e15eb254f944
fynnbe <thefynnbe@gmail.com>
2024-07-17 16:00:47 +0200

    prettify model adapter creation error message

--- bioimageio/core/model_adapters/_model_adapter.py
+++ bioimageio/core/model_adapters/_model_adapter.py
@@ -60,12 +60,17 @@ class ModelAdapter(ABC):
             )
 
         weights = model_description.weights
-        errors: List[str] = []
+        errors: List[Tuple[str, str]] = []
         weight_format_priority_order = (
             DEFAULT_WEIGHT_FORMAT_PRIORITY_ORDER
             if weight_format_priority_order is None
             else weight_format_priority_order
         )
+        # limit weight formats to the ones present
+        weight_format_priority_order = [
+            w for w in weight_format_priority_order if getattr(weights, w) is not None
+        ]
+
         for wf in weight_format_priority_order:
             if wf == "pytorch_state_dict" and weights.pytorch_state_dict is not None:
                 try:
@@ -77,7 +82,7 @@ class ModelAdapter(ABC):
                         devices=devices,
                     )
                 except Exception as e:
-                    errors.append(f"{wf}: {e}")
+                    errors.append((wf, str(e)))
             elif (
                 wf == "tensorflow_saved_model_bundle"
                 and weights.tensorflow_saved_model_bundle is not None
@@ -89,7 +94,7 @@ class ModelAdapter(ABC):
                         model_description=model_description, devices=devices
                     )
                 except Exception as e:
-                    errors.append(f"{wf}: {e}")
+                    errors.append((wf, str(e)))
             elif wf == "onnx" and weights.onnx is not None:
                 try:
                     from ._onnx_model_adapter import ONNXModelAdapter
@@ -98,7 +103,7 @@ class ModelAdapter(ABC):
                         model_description=model_description, devices=devices
                     )
                 except Exception as e:
-                    errors.append(f"{wf}: {e}")
+                    errors.append((wf, str(e)))
             elif wf == "torchscript" and weights.torchscript is not None:
                 try:
                     from ._torchscript_model_adapter import TorchscriptModelAdapter
@@ -107,7 +112,7 @@ class ModelAdapter(ABC):
                         model_description=model_description, devices=devices
                     )
                 except Exception as e:
-                    errors.append(f"{wf}: {e}")
+                    errors.append((wf, str(e)))
             elif wf == "keras_hdf5" and weights.keras_hdf5 is not None:
                 # keras can either be installed as a separate package or used as part of tensorflow
                 # we try to first import the keras model adapter using the separate package and,
@@ -125,15 +130,24 @@ class ModelAdapter(ABC):
                         model_description=model_description, devices=devices
                     )
                 except Exception as e:
-                    errors.append(f"{wf}: {e}")
+                    errors.append((wf, str(e)))
 
         assert errors
-        error_list = "\n - ".join(errors)
-        raise ValueError(
-            "None of the weight format specific model adapters could be created for"
-            + f" '{model_description.id or model_description.name}'"
-            + f" in this environment. Errors are:\n\n{error_list}.\n\n"
-        )
+        if len(weight_format_priority_order) == 1:
+            assert len(errors) == 1
+            raise ValueError(
+                f"The '{weight_format_priority_order[0]}' model adapter could not be created for"
+                + f" '{model_description.id or model_description.name}'"
+                + f" in this environment:\n{errors[0][1]}.\n\n"
+            )
+
+        else:
+            error_list = "\n - ".join(f"{wf}: {e}" for wf, e in errors)
+            raise ValueError(
+                "None of the weight format specific model adapters could be created for"
+                + f" '{model_description.id or model_description.name}'"
+                + f" in this environment. Errors are:\n\n{error_list}.\n\n"
+            )
 
     @final
     def load(self, *, devices: Optional[Sequence[str]] = None) -> None:
