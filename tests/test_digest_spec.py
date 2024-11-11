20545fa54af10c5414baf1c5b9f24c584af7d65b
fynnbe <thefynnbe@gmail.com>
2024-11-11 22:15:41 +0100

    reenable test_get_block_transform

--- tests/test_digest_spec.py
+++ tests/test_digest_spec.py
@@ -1,11 +1,8 @@
-import pytest
-
 from bioimageio.spec import load_description
 from bioimageio.spec.model import v0_5
 
 
 # TODO: don't just test with unet2d_nuclei_broad_model
-@pytest.mark.skip("get_io_sample_block_metas needs improvements")
 def test_get_block_transform(unet2d_nuclei_broad_model: str):
     from bioimageio.core.axis import AxisId
     from bioimageio.core.common import MemberId
@@ -25,18 +22,21 @@ def test_get_block_transform(unet2d_nuclei_broad_model: str):
         if isinstance(a.size, v0_5.ParameterizedSize)
     }
 
+    input_sample_shape = {
+        MemberId("raw"): {
+            AxisId("batch"): 3,
+            AxisId("channel"): 1,
+            AxisId("x"): 4000,
+            AxisId("y"): 3000,
+        }
+    }
+
     _, blocks = get_io_sample_block_metas(
         model,
-        input_sample_shape={
-            MemberId("raw"): {
-                AxisId("batch"): 3,
-                AxisId("channel"): 1,
-                AxisId("x"): 4000,
-                AxisId("y"): 3000,
-            }
-        },
+        input_sample_shape=input_sample_shape,
         ns=ns,
     )
+
     for ipt_block, out_block in blocks:
         trf_block = ipt_block.get_transformed(block_transform)
         assert out_block == trf_block
