7d8e7fc396e899ad693ffe8a7896fde626d41bfd
fynnbe <thefynnbe@gmail.com>
2024-12-11 10:56:43 +0100

    update tests

--- tests/test_weight_converters.py
+++ tests/test_weight_converters.py
@@ -10,25 +10,28 @@ from bioimageio.spec import load_description
 from bioimageio.spec.model import v0_5
 
 
-def test_torchscript_converter(any_torch_model, tmp_path):
+def test_pytorch_to_torchscript(any_torch_model, tmp_path):
     from bioimageio.core.weight_converters.pytorch_to_torchscript import convert
 
-    bio_model = load_description(any_torch_model)
+    model_descr = load_description(any_torch_model)
     out_path = tmp_path / "weights.pt"
-    ret_val = convert(bio_model, out_path)
+    ret_val = convert(model_descr, out_path)
     assert out_path.exists()
     assert isinstance(ret_val, v0_5.TorchscriptWeightsDescr)
     assert ret_val.source == out_path
+    model_descr.weights.torchscript = ret_val
+    summary = test_model(model_descr, weight_format="torchscript")
+    assert summary.status == "passed", summary.format()
 
 
-def test_onnx_converter(convert_to_onnx, tmp_path):
+def test_pytorch_to_onnx(convert_to_onnx, tmp_path):
     from bioimageio.core.weight_converters.pytorch_to_onnx import convert
 
-    bio_model = load_description(convert_to_onnx)
+    model_descr = load_description(convert_to_onnx)
     out_path = tmp_path / "weights.onnx"
     opset_version = 15
     ret_val = convert(
-        model_descr=bio_model,
+        model_descr=model_descr,
         output_path=out_path,
         test_decimal=3,
         opset_version=opset_version,
@@ -37,28 +40,34 @@ def test_onnx_converter(convert_to_onnx, tmp_path):
     assert isinstance(ret_val, v0_5.OnnxWeightsDescr)
     assert ret_val.opset_version == opset_version
     assert ret_val.source == out_path
-    bio_model.weights.onnx = ret_val
-    summary = test_model(bio_model, weights_format="onnx")
+
+    model_descr.weights.onnx = ret_val
+    summary = test_model(model_descr, weight_format="onnx")
     assert summary.status == "passed", summary.format()
 
 
-def test_tensorflow_converter(any_keras_model: Path, tmp_path: Path):
+def test_keras_to_tensorflow(any_keras_model: Path, tmp_path: Path):
     from bioimageio.core.weight_converters.keras_to_tensorflow import convert
 
-    model = load_description(any_keras_model)
-    out_path = tmp_path / "weights.h5"
-    ret_val = convert(model, output_path=out_path)
+    model_descr = load_description(any_keras_model)
+    out_path = tmp_path / "weights"
+    ret_val = convert(model_descr, output_path=out_path)
     assert out_path.exists()
     assert isinstance(ret_val, v0_5.TensorflowSavedModelBundleWeightsDescr)
     assert ret_val.source == out_path
 
+    model_descr.weights.keras = ret_val
+    summary = test_model(model_descr, weight_format="keras_hdf5")
+    assert summary.status == "passed", summary.format()
+
 
 @pytest.mark.skip()
-def test_tensorflow_converter_zipped(any_keras_model: Path, tmp_path: Path):
+def test_keras_to_tensorflow_zipped(any_keras_model: Path, tmp_path: Path):
+    from bioimageio.core.weight_converters.keras_to_tensorflow import convert
+
     out_path = tmp_path / "weights.zip"
-    model = load_description(any_keras_model)
-    util = Tensorflow2Bundled()
-    ret_val = util.convert(model, out_path)
+    model_descr = load_description(any_keras_model)
+    ret_val = convert(model_descr, out_path)
 
     assert out_path.exists()
     assert isinstance(ret_val, v0_5.TensorflowSavedModelBundleWeightsDescr)
@@ -67,3 +76,44 @@ def test_tensorflow_converter_zipped(any_keras_model: Path, tmp_path: Path):
     with zipfile.ZipFile(out_path, "r") as f:
         names = set([name for name in f.namelist()])
     assert len(expected_names - names) == 0
+
+    model_descr.weights.keras = ret_val
+    summary = test_model(model_descr, weight_format="keras_hdf5")
+    assert summary.status == "passed", summary.format()
+
+
+# TODO: add tensorflow_to_keras converter
+# def test_tensorflow_to_keras(any_tensorflow_model: Path, tmp_path: Path):
+#     from bioimageio.core.weight_converters.tensorflow_to_keras import convert
+
+#     model_descr = load_description(any_tensorflow_model)
+#     out_path = tmp_path / "weights.h5"
+#     ret_val = convert(model_descr, output_path=out_path)
+#     assert out_path.exists()
+#     assert isinstance(ret_val, v0_5.TensorflowSavedModelBundleWeightsDescr)
+#     assert ret_val.source == out_path
+
+#     model_descr.weights.keras = ret_val
+#     summary = test_model(model_descr, weight_format="keras_hdf5")
+#     assert summary.status == "passed", summary.format()
+
+
+# @pytest.mark.skip()
+# def test_tensorflow_to_keras_zipped(any_tensorflow_model: Path, tmp_path: Path):
+#     from bioimageio.core.weight_converters.tensorflow_to_keras import convert
+
+#     out_path = tmp_path / "weights.zip"
+#     model_descr = load_description(any_tensorflow_model)
+#     ret_val = convert(model_descr, out_path)
+
+#     assert out_path.exists()
+#     assert isinstance(ret_val, v0_5.TensorflowSavedModelBundleWeightsDescr)
+
+#     expected_names = {"saved_model.pb", "variables/variables.index"}
+#     with zipfile.ZipFile(out_path, "r") as f:
+#         names = set([name for name in f.namelist()])
+#     assert len(expected_names - names) == 0
+
+#     model_descr.weights.keras = ret_val
+#     summary = test_model(model_descr, weight_format="keras_hdf5")
+#     assert summary.status == "passed", summary.format()
