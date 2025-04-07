968317eb9cfc025ea7822b0bb726d33b8fa926cf
fynnbe <thefynnbe@gmail.com>
2025-04-07 15:33:43 +0200

    bump spec

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ _ = setup(
     ],
     packages=find_namespace_packages(exclude=["tests"]),
     install_requires=[
-        "bioimageio.spec ==0.5.4.2",
+        "bioimageio.spec ==0.5.4.3",
         "h5py",
         "imagecodecs",
         "imageio>=2.10",
