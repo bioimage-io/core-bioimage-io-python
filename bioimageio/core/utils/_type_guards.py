aca918647bce93d477f3f395f6ca35a795c31bb5
fynnbe <thefynnbe@gmail.com>
2025-06-13 15:16:57 +0200

    bump pyright

--- bioimageio/core/utils/_type_guards.py
+++ bioimageio/core/utils/_type_guards.py
@@ -3,6 +3,6 @@ They widen the type to T[Any], which is not always correct."""
 
 from bioimageio.spec._internal import type_guards
 
-is_list = type_guards.is_list  # pyright: ignore[reportPrivateImportUsage]
-is_ndarray = type_guards.is_ndarray  # pyright: ignore[reportPrivateImportUsage]
-is_tuple = type_guards.is_tuple  # pyright: ignore[reportPrivateImportUsage]
+is_list = type_guards.is_list
+is_ndarray = type_guards.is_ndarray
+is_tuple = type_guards.is_tuple
