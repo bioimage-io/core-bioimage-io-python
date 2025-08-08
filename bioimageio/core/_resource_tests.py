94f32a53a4b422c3d2fb929184f6de0d7978cadd
fynnbe <thefynnbe@gmail.com>
2025-08-08 10:52:43 +0200

    fix xarray index with boolen array error using numpy instead

--- bioimageio/core/_resource_tests.py
+++ bioimageio/core/_resource_tests.py
@@ -8,6 +8,7 @@ from itertools import product
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import (
+    Any,
     Callable,
     Dict,
     Hashable,
@@ -21,8 +22,10 @@ from typing import (
     overload,
 )
 
+import numpy as np
 import xarray as xr
 from loguru import logger
+from numpy.typing import NDArray
 from typing_extensions import NotRequired, TypedDict, Unpack, assert_never, get_args
 
 from bioimageio.spec import (
@@ -701,45 +704,71 @@ def _test_model_inference(
                     else:
                         continue
 
+                if actual.dims != (dims := expected.dims):
+                    add_error_entry(
+                        f"Output '{m}' has dims {actual.dims}, but expected {expected.dims}"
+                    )
+                    if stop_early:
+                        break
+                    else:
+                        continue
+
+                if actual.tagged_shape != expected.tagged_shape:
+                    add_error_entry(
+                        f"Output '{m}' has shape {actual.tagged_shape}, but expected {expected.tagged_shape}"
+                    )
+                    if stop_early:
+                        break
+                    else:
+                        continue
+
+                expected_np = expected.data.to_numpy().astype(np.float32)
+                del expected
+                actual_np: NDArray[Any] = actual.data.to_numpy().astype(np.float32)
+                del actual
+
                 rtol, atol, mismatched_tol = _get_tolerance(
                     model, wf=weight_format, m=m, **deprecated
                 )
-                rtol_value = rtol * abs(expected)
-                abs_diff = abs(actual - expected)
+                rtol_value = rtol * abs(expected_np)
+                abs_diff = abs(actual_np - expected_np)
                 mismatched = abs_diff > atol + rtol_value
                 mismatched_elements = mismatched.sum().item()
                 if not mismatched_elements:
                     continue
 
-                mismatched_ppm = mismatched_elements / expected.size * 1e6
+                mismatched_ppm = mismatched_elements / expected_np.size * 1e6
                 abs_diff[~mismatched] = 0  # ignore non-mismatched elements
 
-                r_max_idx = (r_diff := (abs_diff / (abs(expected) + 1e-6))).argmax()
+                r_max_idx_flat = (
+                    r_diff := (abs_diff / (abs(expected_np) + 1e-6))
+                ).argmax()
+                r_max_idx = np.unravel_index(r_max_idx_flat, r_diff.shape)
                 r_max = r_diff[r_max_idx].item()
-                r_actual = actual[r_max_idx].item()
-                r_expected = expected[r_max_idx].item()
+                r_actual = actual_np[r_max_idx].item()
+                r_expected = expected_np[r_max_idx].item()
 
                 # Calculate the max absolute difference with the relative tolerance subtracted
-                abs_diff_wo_rtol: xr.DataArray = xr.ufuncs.maximum(
-                    (abs_diff - rtol_value).data, 0
+                abs_diff_wo_rtol: NDArray[np.float32] = (abs_diff - rtol_value).max(
+                    initial=0
                 )
-                a_max_idx = {
-                    AxisId(k): int(v) for k, v in abs_diff_wo_rtol.argmax().items()
-                }
+                a_max_idx = np.unravel_index(
+                    abs_diff_wo_rtol.argmax(), abs_diff_wo_rtol.shape
+                )
 
                 a_max = abs_diff[a_max_idx].item()
-                a_actual = actual[a_max_idx].item()
-                a_expected = expected[a_max_idx].item()
+                a_actual = actual_np[a_max_idx].item()
+                a_expected = expected_np[a_max_idx].item()
 
                 msg = (
                     f"Output '{m}' disagrees with {mismatched_elements} of"
-                    + f" {expected.size} expected values"
+                    + f" {expected_np.size} expected values"
                     + f" ({mismatched_ppm:.1f} ppm)."
                     + f"\n Max relative difference: {r_max:.2e}"
                     + rf" (= \|{r_actual:.2e} - {r_expected:.2e}\|/\|{r_expected:.2e} + 1e-6\|)"
-                    + f" at {r_max_idx}"
+                    + f" at {dict(zip(dims, r_max_idx))}"
                     + f"\n Max absolute difference not accounted for by relative tolerance: {a_max:.2e}"
-                    + rf" (= \|{a_actual:.7e} - {a_expected:.7e}\|) at {a_max_idx}"
+                    + rf" (= \|{a_actual:.7e} - {a_expected:.7e}\|) at {dict(zip(dims, a_max_idx))}"
                 )
                 if mismatched_ppm > mismatched_tol:
                     add_error_entry(msg)
