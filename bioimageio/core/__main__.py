c2cb37084c1c9da3baefdf368bafa923e99cb4ce
fynnbe <thefynnbe@gmail.com>
2025-09-10 14:06:06 +0200

    enable custom logging for CLI

--- bioimageio/core/__main__.py
+++ bioimageio/core/__main__.py
@@ -1,3 +1,18 @@
+import sys
+
+from loguru import logger
+
+logger.enable("bioimageio")
+
+logger.remove()
+_ = logger.add(
+    sys.stderr,
+    level="INFO",
+    format="<green>{elapsed:}</green> | "
+    + "<level>{level: <8}</level> | "
+    + "<cyan>{module}</cyan> - <level>{message}</level>",
+)
+
 from .cli import Bioimageio
 
 
